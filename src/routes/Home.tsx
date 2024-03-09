593f7d12d0e37258615d7404c4e0422eb9128ba6
siggu <siggu100@gmail.com>
2024-03-09 16:22:13 +0900

    Update Home.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,6 +8,25 @@ export default function Home() {
     queryKey: ["comps"],
     queryFn: getComps,
   });
+
+  // 각 origin과 job의 이미지 및 개수를 가져오는 함수
+  const getUniqueImagesWithCount = (champions: any[], key: string) => {
+    const uniqueImagesMap = new Map(); // 이미지를 키로 사용하여 개수를 카운트하는 Map 생성
+
+    champions.forEach(
+      (champion: { [x: string]: { photos: { file: any }[] }[] }) => {
+        const image = champion[key][0].photos[0].file;
+        if (!uniqueImagesMap.has(image)) {
+          uniqueImagesMap.set(image, 1); // 이미지가 없다면 개수를 1로 초기화
+        } else {
+          uniqueImagesMap.set(image, uniqueImagesMap.get(image) + 1); // 이미지가 이미 있다면 개수 증가
+        }
+      }
+    );
+
+    return Array.from(uniqueImagesMap); // Map을 배열로 변환하여 반환
+  };
+
   return (
     <VStack gap={20}>
       <Container maxW={"max-content"}>
@@ -23,15 +42,51 @@ export default function Home() {
           data?.map((comp) => (
             <HStack key={comp.pk}>
               <Text>{comp.name}</Text>
-              {comp.champions.map((champion) => (
-                <Image
-                  bgColor={"gray"}
-                  src={champion.origin[0].photos[0].file}
-                />
-              ))}
-              {comp.champions.map((champion) => (
-                <Image bgColor={"gray"} src={champion.job[0].photos[0].file} />
-              ))}
+              {/* origin 이미지와 개수 표시 */}
+              {getUniqueImagesWithCount(comp.champions, "origin").map(
+                ([image, count], index) => (
+                  <Box
+                    w="45px"
+                    h="45px"
+                    position="relative"
+                    clipPath="polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)"
+                    key={`origin-${index}`}
+                  >
+                    {count >= 7 ? (
+                      <Image bgColor={"#fce47e"} src={image} />
+                    ) : count >= 5 ? (
+                      <Image bgColor={"#bcd8dc"} src={image} />
+                    ) : count >= 3 ? (
+                      <Image bgColor={"#d07c47"} src={image} />
+                    ) : (
+                      <Image bgColor={"gray"} src={image} />
+                    )}
+                  </Box>
+                )
+              )}
+              {/* job 이미지와 개수 표시 */}
+              {getUniqueImagesWithCount(comp.champions, "job").map(
+                ([image, count], index) => (
+                  <Box
+                    w="45px"
+                    h="45px"
+                    position="relative"
+                    clipPath="polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)"
+                    key={`origin-${index}`}
+                  >
+                    {count >= 5 ? (
+                      <Image bgColor={"#fce47e"} src={image} />
+                    ) : count >= 3 ? (
+                      <Image bgColor={"#bcd8dc"} src={image} />
+                    ) : count >= 2 ? (
+                      <Image bgColor={"#d07c47"} src={image} />
+                    ) : (
+                      <Image bgColor={"gray"} src={image} />
+                    )}
+                  </Box>
+                )
+              )}
+              {/* 챔피언 이미지 표시 */}
               {comp.champions
                 .slice()
                 .sort((a, b) => a.cost - b.cost)

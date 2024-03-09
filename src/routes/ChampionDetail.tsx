6cb14051d0a563276cf20d382cf988f8d4a0b8bf
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-03-10 01:05:28 +0900

    챔피언 디테일 80% 완성
    1. 1~3성 별 판매 비용 표시여부
    2. 1~3성 별 체력, 공격력, DPS 여부(modle수정필요해보임)

--- src/routes/ChampionDetail.tsx
+++ src/routes/ChampionDetail.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getChampion } from "../api";
 import { useParams } from "react-router-dom";
-import { IoIosFlask } from "react-icons/io";
+import { FaFlask,FaCoins } from "react-icons/fa";
 import { Box, Container, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import IChampionDetail from "../components/types";
 
@@ -12,18 +12,41 @@ export default function ChampionDetail() {
     queryFn: getChampion,
   });
   return (
-    <VStack gap={0} justifyContent={"center"} pl="10%" pr="10%">
-      <HStack width="100%" alignItems={"flex-start"}border={"solid"} borderColor='gray.300' borderWidth={1} p={5} gap={5} borderBottom={0} backgroundColor="black" color="white">
+    <VStack gap={0} justifyContent={"center"} pl="10%" pr="10%" fontSize={"sm"}>
+
         {/* 이름, 초상화, 비용, 계열, 직업 */}
-        <Image rounded={"2xl"} src={data?.photos[0].file} />
-        <Text>{data?.name}</Text>
-        <Text>${data?.cost}</Text>
+      <HStack width="100%" justifyContent={"space-between"} border={"solid"} borderColor='gray.300' borderWidth={1} gap={5} borderBottom={0} backgroundColor="black" color="white">
+        <HStack p={5}>
+          <Image src={data?.photos[0].file} borderRadius={"20"} />
+          <Text p={5} fontSize={"2xl"} as={"b"}>{data?.name}</Text>
+        </HStack>
+        <VStack p={5} alignItems={"flex-start"} bgColor={"#2b2e37"} >
+          <HStack>
+            <Text>비용</Text>
+            <FaCoins color="#ffb42c"/>
+            <Text>{data?.cost}</Text>
+          </HStack>
+        <HStack >
+          {data?.origin.length === 1 ? (
+            <>
+              <Image w="45px" h="45px"rounded={"2xl"} bgColor={"gray"}
+                src={data?.origin[0].photos[0].file}/>
+              <Text>{data?.origin[0].name}</Text>
+            </>
+          ) : (
+            [0, 1].map((index) => (
+              <>
+                <Image w="45px" h="45px"rounded={"2xl"} bgColor={"gray"}
+                  src={data?.origin[index].photos[0].file}/>
+                <Text>{data?.origin[index].name}</Text>
+              </>
+            ))
+          )}
+        </HStack>
         <HStack >
           {data?.job.length === 1 ? (
             <>
-              <Image fill={"black"}
-                rounded={"2xl"}
-                bgColor={"gray"}
+              <Image w="45px" h="45px"rounded={"2xl"} bgColor={"gray"}
                 src={data?.job[0].photos[0].file}
               />
               <Text>{data?.job[0].name}</Text>
@@ -31,9 +54,7 @@ export default function ChampionDetail() {
           ) : (
             [0, 1].map((index) => (
               <>
-                <Image
-                  rounded={"2xl"}
-                  bgColor={"gray"}
+                <Image w="45px" h="45px"rounded={"2xl"} bgColor={"gray"}
                   src={data?.job[index].photos[0].file}
                 />
                 <Text>{data?.job[index].name}</Text>
@@ -41,66 +62,46 @@ export default function ChampionDetail() {
             ))
           )}
         </HStack>
-        <HStack>
-          {data?.origin.length === 1 ? (
-            <>
-              <Image
-                rounded={"2xl"}
-                bgColor={"gray"}
-                src={data?.origin[0].photos[0].file}
-              />
-              <Text>{data?.origin[0].name}</Text>
-            </>
-          ) : (
-            [0, 1].map((index) => (
-              <>
-                <Image
-                  rounded={"2xl"}
-                  bgColor={"gray"}
-                  src={data?.origin[index].photos[0].file}
-                />
-                <Text>{data?.origin[index].name}</Text>
-              </>
-            ))
-          )}
-        </HStack>
+        </VStack>
+
       </HStack>
-      <HStack width="100%" alignItems={"flex-start"}border={"solid"} borderColor='gray.300' borderWidth={1} p={5} gap={5} borderBottom={0}>
+
         {/* 체력, 공격력, DPS, 공격사거리, 공격속도, 방어력, 마법저항력 */}
+      <HStack width="100%" alignItems={"flex-start"}border={"solid"} 
+              borderColor='gray.300' borderWidth={1} p={5} gap={5} 
+              borderBottom={0} justifyContent={"space-evenly"}>
         <VStack>
           <Text>체력</Text>
-          <Text>{data?.health}</Text>
+          <Text as={"b"}>{data?.health}</Text>
         </VStack>
         <VStack>
         <Text>공격력</Text>
-        <Text>{data?.ad}</Text>
+        <Text as={"b"}>{data?.ad}</Text>
         </VStack>
         <VStack>
         <Text>DPS</Text>
-        <Text>{data?.dps}</Text>
+        <Text as={"b"}>{data?.dps}</Text>
           </VStack>
           <VStack>
           <Text>공격사거리</Text>
-          <Text>{data?.attack_range}</Text>
+          <Text as={"b"}>{data?.attack_range}</Text>
           </VStack>
           <VStack>
           <Text>공격속도</Text>
-          
-        <Text>{data?.attack_speed}</Text>
+        <Text as={"b"}>{data?.attack_speed}</Text>
           </VStack>
           <VStack>
           <Text>방어력</Text>
-        
-        <Text>{data?.armor}</Text>
+        <Text as={"b"}>{data?.armor}</Text>
           </VStack>
           <VStack>
           <Text>마법저항력</Text>
-          <Text>{data?.magic_resistance}</Text>
+          <Text as={"b"}>{data?.magic_resistance}</Text>
           </VStack>
       </HStack>
 
-      <HStack width="100%" alignItems={"flex-start"}border={"solid"} borderColor='gray.300' borderWidth={1} p={5} gap={5}>
         {/* 스킬 초상화, 스킬명, 스킬유형, 마나, 스킬 내용, 스킬효과 */}
+      <HStack width="100%" alignItems={"flex-start"}border={"solid"} borderColor='gray.300' borderWidth={1} p={5} gap={5}>
         
         <Image src={data?.skill.photos[0].file}/>
         
@@ -109,7 +110,7 @@ export default function ChampionDetail() {
           <HStack>
           <Text>{data?.skill.skill_type}</Text>
           <Text>|</Text>
-          <IoIosFlask color="#217ac7"/>
+          <FaFlask color="#217ac7"/>
           <Text>{data?.skill.start_mana}/{data?.skill.max_mana}</Text>
           </HStack>
           <Text>{data?.skill.description}</Text>

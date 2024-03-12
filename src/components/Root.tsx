72e367bb10e2365e749109ca33ed0e69ef28ef17
siggu <siggu100@gmail.com>
2024-03-12 16:23:35 +0900

    추천 메타

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Root() {
   return (
     <h1>
       i'm root
       <Outlet />
+      <ReactQueryDevtools />
     </h1>
   );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,43 +12,245 @@ import { useQuery } from "@tanstack/react-query";
 import { getComps } from "../api";
 import IComp from "../components/types";
 import { FaCoins } from "react-icons/fa";
-import { useState } from "react";
 
 export default function Home() {
   const { data, isLoading } = useQuery<IComp[]>({
     queryKey: ["comps"],
     queryFn: getComps,
   });
+  const originStack: string[] = [];
+  const jobStack: string[] = [];
+  const originCounts: { [key: string]: number } = {};
+  const jobCounts: { [key: string]: number } = {};
 
+  // origin과 job의 총 개수 저장
+  const originAndJobCounts: { [key: string]: number } = {};
+
+  if (!isLoading && data) {
+    data.forEach((comp) => {
+      comp.champions.forEach((champion) => {
+        // origin 개수 계산
+        champion.origin.forEach((origin: { name: string | number }) => {
+          if (originAndJobCounts[origin.name]) {
+            originAndJobCounts[origin.name]++;
+          } else {
+            originAndJobCounts[origin.name] = 1;
+          }
+        });
+
+        // job 개수 계산
+        champion.job.forEach((job: { name: string | number }) => {
+          if (originAndJobCounts[job.name]) {
+            originAndJobCounts[job.name]++;
+          } else {
+            originAndJobCounts[job.name] = 1;
+          }
+        });
+      });
+    });
+  }
+  const sortedOriginAndJobCounts = Object.entries(originAndJobCounts).sort(
+    (a, b) => b[1] - a[1]
+  );
   // console.log(data?.[0].champions[0].origin[0].photos[0].file);
-
+  // console.log(originAndJobCounts);
   return (
     <VStack gap={20}>
       <Container maxW={"max-content"}>
         <Text>검색창</Text>
       </Container>
       <Container maxW={"container.xl"}>
-        <Box>
-          <h3>추천메타</h3>
+        <Box mb={5}>
+          <Text as={"b"} color={"red"} fontSize={"20px"}>
+            추천 메타
+          </Text>
         </Box>
         {isLoading ? (
           <Text>Loading...</Text>
         ) : (
           data?.map((comp) => (
-            <HStack key={comp.pk} gap={5}>
-              <Text fontSize={"20px"} as={"b"}>
-                {comp.name}
-              </Text>
+            <HStack
+              position={"relative"}
+              bg={"gray.700"}
+              p={5}
+              m={2}
+              key={comp.pk}
+              gap={10}
+            >
+              <Box w={"150px"}>
+                <Text color={"white"} fontSize={"15px"}>
+                  {comp.name}
+                </Text>
+              </Box>
               <VStack gap={3} alignItems={"flex-start"}>
-                <HStack>
+                <HStack gap={0}>
                   {comp.champions.map((champion) => (
                     <>
-                      <Text></Text>
-                      <Image
-                        rounded={"2xl"}
-                        bg={"gray"}
-                        src={champion.origin[0].photos[0].file}
-                      />
+                      {champion.origin.map(
+                        (
+                          origin: {
+                            name: string;
+                            description: string;
+                            effect: string;
+                            photos: { file: string | undefined }[];
+                          },
+                          index: any
+                        ) => {
+                          if (originStack.includes(origin.name)) return null;
+                          originStack.push(origin.name);
+                          return (
+                            <Tooltip
+                              hasArrow
+                              w={"300px"}
+                              label={
+                                <VStack
+                                  gap={3}
+                                  alignItems={"flex-start"}
+                                  w={"280px"}
+                                >
+                                  <HStack>
+                                    <Text as={"b"}>{origin.name}</Text>
+                                  </HStack>
+                                  <VStack>
+                                    <Text>{origin.description}</Text>
+                                  </VStack>
+                                  <VStack>
+                                    <Text>{origin.effect}</Text>
+                                  </VStack>
+                                </VStack>
+                              }
+                              bg={"black"}
+                              rounded={"md"}
+                              p={3}
+                            >
+                              <HStack>
+                                <Image
+                                  key={`${comp.pk}-${champion.pk}-origin-${index}`}
+                                  p={1}
+                                  w={"30px"}
+                                  rounded={"10px"}
+                                  bg={
+                                    originAndJobCounts[origin.name] >= 7
+                                      ? "orange"
+                                      : originAndJobCounts[origin.name] >= 5
+                                      ? "silver"
+                                      : originAndJobCounts[origin.name] > 3
+                                      ? "brown"
+                                      : "gray"
+                                  }
+                                  src={origin.photos[0].file}
+                                  position={"relative"}
+                                />
+                                <Box
+                                  left={-3}
+                                  h={"20px"}
+                                  pr={1}
+                                  pl={1}
+                                  pb={6}
+                                  roundedRight={"5px"}
+                                  position={"relative"}
+                                  color={"white"}
+                                  bg={
+                                    originAndJobCounts[origin.name] >= 7
+                                      ? "orange"
+                                      : originAndJobCounts[origin.name] >= 5
+                                      ? "silver"
+                                      : originAndJobCounts[origin.name] > 3
+                                      ? "brown"
+                                      : "gray"
+                                  }
+                                >
+                                  <Text fontSize={"13px"}>
+                                    {originAndJobCounts[origin.name]}
+                                  </Text>
+                                </Box>
+                              </HStack>
+                            </Tooltip>
+                          );
+                        }
+                      )}
+                      {champion.job.map(
+                        (
+                          job: {
+                            name: string;
+                            description: string;
+                            effect: string;
+                            photos: { file: string | undefined }[];
+                          },
+                          index: any
+                        ) => {
+                          if (jobStack.includes(job.name)) return null;
+                          jobStack.push(job.name);
+                          return (
+                            <Tooltip
+                              hasArrow
+                              w={"300px"}
+                              label={
+                                <VStack
+                                  gap={3}
+                                  alignItems={"flex-start"}
+                                  w={"280px"}
+                                >
+                                  <HStack>
+                                    <Text as={"b"}>{job.name}</Text>
+                                  </HStack>
+                                  <VStack>
+                                    <Text>{job.description}</Text>
+                                  </VStack>
+                                  <VStack>
+                                    <Text>{job.effect}</Text>
+                                  </VStack>
+                                </VStack>
+                              }
+                              bg={"black"}
+                              rounded={"md"}
+                              p={3}
+                            >
+                              <HStack>
+                                <Image
+                                  key={`${comp.pk}-${champion.pk}-job-${index}`}
+                                  p={1}
+                                  w={"30px"}
+                                  rounded={"10px"}
+                                  bg={
+                                    originAndJobCounts[job.name] >= 7
+                                      ? "gold"
+                                      : originAndJobCounts[job.name] >= 5
+                                      ? "silver"
+                                      : originAndJobCounts[job.name] >= 2
+                                      ? "brown"
+                                      : "gray"
+                                  }
+                                  src={job.photos[0].file}
+                                />
+                                <Box
+                                  left={-3}
+                                  h={"20px"}
+                                  pr={1}
+                                  pl={1}
+                                  pb={6}
+                                  roundedRight={"5px"}
+                                  position={"relative"}
+                                  color={"white"}
+                                  bg={
+                                    originAndJobCounts[job.name] >= 7
+                                      ? "gold"
+                                      : originAndJobCounts[job.name] >= 5
+                                      ? "silver"
+                                      : originAndJobCounts[job.name] >= 2
+                                      ? "brown"
+                                      : "gray"
+                                  }
+                                >
+                                  <Text fontSize={"13px"}>
+                                    {originAndJobCounts[job.name]}
+                                  </Text>
+                                </Box>
+                              </HStack>
+                            </Tooltip>
+                          );
+                        }
+                      )}
                     </>
                   ))}
                 </HStack>
@@ -179,6 +381,16 @@ export default function Home() {
                     ))}
                 </HStack>
               </VStack>
+              <Box
+                position={"absolute"}
+                right={5}
+                p={3}
+                border={"1px solid white"}
+              >
+                <Text color={"white"} fontSize={"13px"}>
+                  공략 더 보기
+                </Text>
+              </Box>
             </HStack>
           ))
         )}

d12cae0b18e5c5621d759cb72866caf7bede3859
siggu <106001755+siggu@users.noreply.github.com>
2024-05-17 14:12:30 +0900

    synergies 완료

--- src/routes/Synergies.tsx
+++ src/routes/Synergies.tsx
@@ -1,44 +1,123 @@
 import {useQuery} from '@tanstack/react-query';
 import {getChampions, getSynergies} from '../api';
-import {
-  Container,
-  VStack,
-  Text,
-  Grid,
-  Box,
-  Image,
-  HStack,
-  Tooltip,
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from '@chakra-ui/react';
+import {Container, VStack, Text, Grid, Box, Image, HStack, Tooltip, GridItem} from '@chakra-ui/react';
 import ISynergy from '../components/types';
 import IChampionDetail from './../components/types.d';
-import React from 'react';
 import Champion from '../components/Champion';
 
 export default function Synergies() {
   const {data: synergiesData, isLoading: isSynergiesLoading} = useQuery<ISynergy[]>({
-    queryKey: ['origins'],
+    queryKey: ['synergies'],
     queryFn: getSynergies,
   });
   const {data: championsData, isLoading: isChampionsLoading} = useQuery<IChampionDetail[]>({
     queryKey: ['champions'],
     queryFn: getChampions,
   });
-  championsData?.sort((a, b) => a.cost - b.cost);
-  console.log(championsData);
-  console.log(synergiesData);
 
   if (isSynergiesLoading || isChampionsLoading) return <div>Loading...</div>;
-  if (!isSynergiesLoading || !!championsData) return <div>Data not available</div>;
+  if (!synergiesData || !championsData) return <div>Data not available</div>;
 
-  return <div></div>;
+  // championsData를 cost1을 기준으로 정렬
+  championsData.sort((a, b) => a.cost1 - b.cost1);
+
+  return (
+    <Container p={5} maxW={'container.xl'}>
+      <Box color={'white'}>
+        <Grid
+          templateColumns={{
+            sm: 'repeat(1, 1fr)',
+            lg: 'repeat(2, 1fr)',
+          }}
+          gap={10}
+        >
+          {synergiesData?.map((synergy: any) => (
+            <Box key={synergy.name} p={5} border={'1px solid gray'} borderRadius={5}>
+              <HStack justifyContent={'center'} mb={3} borderBottom={'1px solid gray'}>
+                <HStack mb={3}>
+                  <Image w={'40px'} src={synergy?.whiteImageUrl} />
+                  <Text as={'b'}>{synergy.name}</Text>
+                </HStack>
+              </HStack>
+              <Text
+                mb={5}
+                dangerouslySetInnerHTML={{
+                  __html: synergy.desc.replace(/<br>/g, '<br />'),
+                }}
+              />
+              <Box mb={10}>
+                {Array.from({length: 6}).map((_, index) => {
+                  const statKey = `stats${index + 1}`;
+                  if (synergy?.[statKey]) {
+                    return (
+                      <Text
+                        key={index}
+                        color={'gray'}
+                        dangerouslySetInnerHTML={{__html: synergy[statKey].replace(/<br>/g, '<br />')}}
+                      />
+                    );
+                  }
+                  return null;
+                })}
+              </Box>
+              <Grid alignItems={'center'} gap={5} templateColumns="repeat(5, 1fr)" mt={3}>
+                {championsData
+                  .filter((champion) =>
+                    [champion.traits1, champion.traits2, champion.traits3, champion.traits4].includes(synergy.key)
+                  )
+                  .map((champion) => (
+                    <Champion
+                      key={champion.key}
+                      championKey={champion.key}
+                      name={champion.name}
+                      cost1={champion.cost1}
+                      imageUrl={champion.imageUrl}
+                      attackRange={champion.attackRange}
+                      ingameKey={champion.ingameKey}
+                      splashUrl={champion.splashUrl}
+                      traits1={champion.traits1}
+                      traits2={champion.traits2}
+                      traits3={champion.traits3}
+                      traits4={champion.traits4}
+                      isHiddenGuide={champion.isHiddenGuide}
+                      isHiddenLanding={champion.isHiddenLanding}
+                      isHiddenTeamBuiler={champion.isHiddenTeamBuiler}
+                      cost2={champion.cost2}
+                      cost3={champion.cost3}
+                      health1={champion.health1}
+                      health2={champion.health2}
+                      health3={champion.health3}
+                      attackDamage1={champion.attackDamage1}
+                      attackDamage2={champion.attackDamage2}
+                      attackDamage3={champion.attackDamage3}
+                      damagePerSecond1={champion.damagePerSecond1}
+                      damagePerSecond2={champion.damagePerSecond2}
+                      damagePerSecond3={champion.damagePerSecond3}
+                      attackSpeed={champion.attackSpeed}
+                      armor={champion.armor}
+                      magicalResistance={0}
+                      recommendItems1={champion.recommendItems1}
+                      recommendItems2={champion.recommendItems2}
+                      recommendItems3={champion.recommendItems3}
+                      recommendItems4={champion.recommendItems4}
+                      recommendItems5={champion.recommendItems5}
+                      skill_stats1={champion.skill_stats1}
+                      skill_stats2={champion.skill_stats2}
+                      skill_stats3={champion.skill_stats3}
+                      skill_stats4={champion.skill_stats4}
+                      skill_stats5={champion.skill_stats5}
+                      skill_name={champion.skill_name}
+                      skill_imageUrl={champion.skill_imageUrl}
+                      skill_desc={champion.skill_desc}
+                      skill_startingMana={champion.skill_startingMana}
+                      skill_skillMana={champion.skill_skillMana}
+                    />
+                  ))}
+              </Grid>
+            </Box>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
 }

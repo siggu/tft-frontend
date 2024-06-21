5b7c7c7600473b57a99927cd91375c724d2be56e
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-06-21 20:13:06 +0900

    리렌더링 성공

--- src/routes/ProfileSearchAdapter.tsx
+++ src/routes/ProfileSearchAdapter.tsx
@@ -1,25 +1,50 @@
-import {useQuery} from '@tanstack/react-query';
-import {Box, Grid, Text, VStack, Image, HStack, Container, Skeleton, SkeletonText} from '@chakra-ui/react';
-import ProfileMiniBox from '../components/ProfileMiniBox';
-import {useParams} from 'react-router-dom';
-import IProfileMiniBox from '../components/types';
-import {getSummonerData, postSummonerData} from '../api';
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Box,
+  Grid,
+  Text,
+  VStack,
+  Image,
+  HStack,
+  Container,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
+import ProfileMiniBox from "../components/ProfileMiniBox";
+import { useParams } from "react-router-dom";
+import IProfileMiniBox from "../components/types";
+import { getSummonerData, postSummonerData } from "../api";
 
 export default function ProfileSearchAdapter() {
-  const {gameName, tagLine} = useParams();
-  const {data: summonerData, isLoading: isSummonerDataLoading} = useQuery<IProfileMiniBox>({
-    queryKey: ['', gameName, tagLine],
-    queryFn: getSummonerData,
-  });
+  const { gameName, tagLine } = useParams();
+  const { data: summonerData, isLoading: isSummonerDataLoading } =
+    useQuery<IProfileMiniBox>({
+      queryKey: ["", gameName, tagLine],
+      queryFn: getSummonerData,
+    });
+  // 로컬 state로 summonerData를 관리
+  const [localSummonerData, setLocalSummonerData] = useState(summonerData);
+
+  // summonerData가 변경될 때마다 로컬 state를 업데이트
+  useEffect(() => {
+    if (summonerData) {
+      setLocalSummonerData(summonerData);
+    }
+  }, [summonerData]);
+
+  console.log("summonerData", summonerData);
+  console.log("isSummonerDataLoading", isSummonerDataLoading);
+  console.log("localSummonerData", localSummonerData);
 
   return (
     <VStack>
-      {!summonerData ? (
-        <HStack w={'400px'} h={'200px'} mb={'300px'}>
-          <Skeleton borderRadius={'10px'} w={'70px'} h={'70px'} />
+      {!localSummonerData ? (
+        <HStack w={"400px"} h={"200px"} mb={"300px"}>
+          <Skeleton borderRadius={"10px"} w={"70px"} h={"70px"} />
           <VStack>
-            <HStack color={'white'}>
-              <SkeletonText fontSize={'24px'}>{gameName}</SkeletonText>
+            <HStack color={"white"}>
+              <SkeletonText fontSize={"24px"}>{gameName}</SkeletonText>
             </HStack>
           </VStack>
         </HStack>

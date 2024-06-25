db0d9ccb8f4b0e36fc75354c30496773c2335210
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-06-25 16:21:56 +0900

    해치웠나?

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,22 +1,11 @@
-import {
-  Box,
-  Button,
-  Container,
-  HStack,
-  Heading,
-  Input,
-  Image,
-  Text,
-  VStack,
-  SkeletonText,
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { useLocation, useParams } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import ILeagueEntryDTO from "../components/types";
-import IProfileMiniBox from "../components/types";
-import { getLeagueEntries, getMatchesByPuuid, getSummonerData } from "../api";
+import { Box, Button, Container, HStack, Heading, Input, Image, Text, VStack, SkeletonText } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useLocation, useParams } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+import ILeagueEntryDTO from '../components/types';
+import IProfileMiniBox from '../components/types';
+import { getLeagueEntries, getMatchesByPuuid, getSummonerData } from '../api';
 
 interface IProfile {
   accountId: string;
@@ -164,23 +153,20 @@ interface IProfileId {
 
 export default function Profile() {
   const { gameName, tagLine } = useParams();
-  const { data: summonerData, isLoading: isSummonerDataLoading } =
-    useQuery<IProfileMiniBox>({
-      queryKey: ["", gameName, tagLine],
-      queryFn: getSummonerData,
-    });
+  const { data: summonerData, isLoading: isSummonerDataLoading } = useQuery<IProfileMiniBox>({
+    queryKey: ['', gameName, tagLine],
+    queryFn: getSummonerData,
+  });
   const summonerId = summonerData?.summonerId;
   const puuid = summonerData?.puuid;
-  const { data: leagueEntryData, isLoading: isLeagueEntryDataLoading } =
-    useQuery<ILeagueEntryDTO>({
-      queryKey: ["", summonerId],
-      queryFn: getLeagueEntries,
-    });
-  const { data: matchesByPuuidData, isLoading: ismatchByPuuidDataLoading } =
-    useQuery<ILeagueEntryDTO>({
-      queryKey: ["", puuid],
-      queryFn: getMatchesByPuuid,
-    });
+  const { data: leagueEntryData, isLoading: isLeagueEntryDataLoading } = useQuery<ILeagueEntryDTO>({
+    queryKey: ['', summonerId],
+    queryFn: getLeagueEntries,
+  });
+  const { data: matchesByPuuidData, isLoading: ismatchByPuuidDataLoading } = useQuery<ILeagueEntryDTO>({
+    queryKey: ['', puuid],
+    queryFn: getMatchesByPuuid,
+  });
 
   const location = useLocation();
   const { name, matches } = location.state || {}; // 기본값 설정
@@ -189,67 +175,26 @@ export default function Profile() {
   // console.log('Matches:', matches);
   const [matchData, setMatchData] = useState<IMatch[]>([]);
 
-  // const handleSearch = async () => {
-  //   try {
-  //     const responses = await Promise.all(
-  //       matches.map(async (match: any) => {
-  //         const response = await fetch(
-  //           `http://127.0.0.1:8000/api/v1/profiles/matches-by-puuid/${name}/${match.match_id}`,
-  //           {
-  //             method: 'GET',
-  //             headers: {
-  //               'Content-Type': 'application/json',
-  //             },
-  //           }
-  //         );
-  //         return response.json();
-  //       })
-  //     );
-  //     if (responses) {
-  //       setMatchData(responses);
-  //       console.log(matchData);
-  //     } else {
-  //       const responses = await Promise.all(
-  //         matches.map(async (match: any) => {
-  //           const response = await fetch(
-  //             `http://127.0.0.1:8000/api/v1/profiles/matches-by-puuid/${name}/${match.match_id}`,
-  //             {
-  //               method: 'POST',
-  //               headers: {
-  //                 'Content-Type': 'application/json',
-  //               },
-  //               body: JSON.stringify({summonerName: name, matchId: match.match_id}),
-  //             }
-  //           );
-  //           return response.json();
-  //         })
-  //       );
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   return (
     <VStack>
-      <Container maxW={"container.xl"}>
-        <HStack color={"white"}>
+      <Container maxW={'container.xl'}>
+        <HStack color={'white'}>
           <Image
-            borderStyle={"10px black solid"}
-            borderRadius={"full"}
-            w={"100px"}
-            h={"100px"}
+            borderStyle={'10px black solid'}
+            borderRadius={'full'}
+            w={'100px'}
+            h={'100px'}
             src={`https://ddragon.leagueoflegends.com/cdn/14.12.1/img/profileicon/${summonerData?.profileIconId}.png`}
           />
-          <VStack alignItems={"flex-start"}>
+          <VStack alignItems={'flex-start'}>
             <HStack>
               <Text>{summonerData?.gameName}</Text>
               <Text>{summonerData?.tagLine}</Text>
             </HStack>
             {isLeagueEntryDataLoading && !leagueEntryData ? (
               <HStack>
-                <SkeletonText fontSize={"24px"}></SkeletonText>
-                <SkeletonText fontSize={"24px"}></SkeletonText>
+                <SkeletonText fontSize={'24px'}></SkeletonText>
+                <SkeletonText fontSize={'24px'}></SkeletonText>
               </HStack>
             ) : (
               <>
@@ -277,108 +222,86 @@ export default function Profile() {
           </VStack>
         </HStack>
         <Box>
-          <Text fontSize={"20px"} as={"b"} color={"#dca555"}>
+          <Text fontSize={'20px'} as={'b'} color={'#dca555'}>
             유저 전적
           </Text>
         </Box>
         <Box>
           <Box>
             <HStack>
-              <Text color={"white"}>매치 데이터:</Text>
-              {/* <Button type="button" onClick={handleSearch}> */}
-              <Button type="button">
-                <FaSearch />
-              </Button>
+              <Text color={'white'}>매치 데이터:</Text>
             </HStack>
+            <>{console.log(matchData)}</>
             {matchData.map((match) => (
               <Box>
                 {match.metadata.participants.map((participant, index) => (
                   <>
-                    <Box mb={10} border={"1px solid white"} p={5}>
+                    <Box mb={10} border={'1px solid white'} p={5}>
                       {/* 증강 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>증강</Text>
-                        {match.info.participants[index].augments.map(
-                          (augment) => (
-                            <Text color={"white"}>{augment}</Text>
-                          )
-                        )}
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>증강</Text>
+                        {match.info.participants[index].augments.map((augment) => (
+                          <Text color={'white'}>{augment}</Text>
+                        ))}
                       </Box>
                       {/* 전설이 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
-                          전설이:{" "}
-                          {match.info.participants[index].companion.content_ID}
-                        </Text>
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>전설이: {match.info.participants[index].companion.content_ID}</Text>
                       </Box>
                       {/* 남은 골드 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
-                          남은 골드: {match.info.participants[index].gold_left}
-                        </Text>
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>남은 골드: {match.info.participants[index].gold_left}</Text>
                       </Box>
                       {/* 레벨 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
-                          레벨: {match.info.participants[index].level}
-                        </Text>
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>레벨: {match.info.participants[index].level}</Text>
                       </Box>
                       {/* 등수 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
-                          등수: {match.info.participants[index].placement}
-                        </Text>
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>등수: {match.info.participants[index].placement}</Text>
                       </Box>
                       {/* 처치한 플레이어 수 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
-                          처치한 플레이어 수:{" "}
-                          {match.info.participants[index].players_eliminated}
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>
+                          처치한 플레이어 수: {match.info.participants[index].players_eliminated}
                         </Text>
                       </Box>
                       {/* 플레이어에게 가한 대미지 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
-                          플레이어에게 가한 대미지:{" "}
-                          {
-                            match.info.participants[index]
-                              .total_damage_to_players
-                          }
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>
+                          플레이어에게 가한 대미지: {match.info.participants[index].total_damage_to_players}
                         </Text>
                       </Box>
                       {/* 시너지 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>시너지</Text>
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>시너지</Text>
                         {match.info.participants[index].traits.map((trait) => (
-                          <Text color={"white"}>
-                            {trait.name}, {trait.num_units},{" "}
-                            {trait.tier_current}, {trait.tier_total}
+                          <Text color={'white'}>
+                            {trait.name}, {trait.num_units}, {trait.tier_current}, {trait.tier_total}
                           </Text>
                         ))}
                       </Box>
                       {/* 사용한 챔피언 */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>챔피언</Text>
-                        {match.info.participants[index].units.map(
-                          (champion) => (
-                            <>
-                              <Box border={"1px solid white"} p={3}>
-                                <Text color={"white"}>
-                                  {champion.tier}성 {champion.character_id}
-                                </Text>
-                                {/* 아이템 목록 */}
-                                <Text color={"white"}>아이템 목록:</Text>
-                                {champion.itemNames?.map((item) => (
-                                  <Text color={"white"}>{item}</Text>
-                                ))}
-                              </Box>
-                            </>
-                          )
-                        )}
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>챔피언</Text>
+                        {match.info.participants[index].units.map((champion) => (
+                          <>
+                            <Box border={'1px solid white'} p={3}>
+                              <Text color={'white'}>
+                                {champion.tier}성 {champion.character_id}
+                              </Text>
+                              {/* 아이템 목록 */}
+                              <Text color={'white'}>아이템 목록:</Text>
+                              {champion.itemNames?.map((item) => (
+                                <Text color={'white'}>{item}</Text>
+                              ))}
+                            </Box>
+                          </>
+                        ))}
                       </Box>
                       {/* 유저 puuid */}
-                      <Box border={"1px solid white"} p={3}>
-                        <Text color={"white"}>
+                      <Box border={'1px solid white'} p={3}>
+                        <Text color={'white'}>
                           유저 puuid: <br></br>
                         </Text>
                       </Box>

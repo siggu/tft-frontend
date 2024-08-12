2786fcf79f2675dcdf26280770e574ce34c6e93f
siggu <106001755+siggu@users.noreply.github.com>
2024-08-12 14:37:05 +0900

    helmet 추가
    favicon.ico 변경

--- src/components/set12/Set12Root.tsx
+++ src/components/set12/Set12Root.tsx
@@ -3,10 +3,12 @@ import { Outlet } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Box } from '@chakra-ui/react';
 import Header from './Set12Header';
+import { Helmet } from 'react-helmet';
 
 export default function Set12Root() {
   return (
     <Box bg={'#161618'}>
+      <Helmet>reroll - Set12</Helmet>
       <Header />
       <Outlet />
       <ReactQueryDevtools />
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import router from './router';
+import { Helmet } from 'react-helmet';
 
 const client = new QueryClient();
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-  <QueryClientProvider client={client}>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </QueryClientProvider>
+  <>
+    <Helmet>
+      <title>reroll</title>
+    </Helmet>
+    <QueryClientProvider client={client}>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </>
 );

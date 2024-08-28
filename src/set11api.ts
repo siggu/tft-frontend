a07242b79db133e4ec0e99e10adca495f6687521
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-08-28 19:17:25 +0900

    backend.reroll.lol

--- src/set11api.ts
+++ src/set11api.ts
@@ -4,9 +4,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://127.0.0.1:8000/api/v1/'
-      : 'https://tft-backend-h1wi.onrender.com/api/v1/',
+    process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/v1/' : 'https://backend.reroll.lol/api/v1/',
   withCredentials: true, // 백엔드 서버로부터 쿠키를 허용
 });
 // 증강

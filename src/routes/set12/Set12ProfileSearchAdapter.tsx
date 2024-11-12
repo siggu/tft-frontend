0cd9d8436d1c83b0c3f2c7c5438b649a9e1c6e04
siggu <106001755+siggu@users.noreply.github.com>
2024-11-12 11:58:32 +0900

    유저 검색 결과 화면 조정

--- src/routes/set12/Set12ProfileSearchAdapter.tsx
+++ src/routes/set12/Set12ProfileSearchAdapter.tsx
@@ -17,27 +17,29 @@ export default function Set11ProfileSearchAdapter() {
   // console.log('isSummonerDataLoading', isSummonerDataLoading);
 
   return (
-    <VStack>
-      {isSummonerDataLoading ? (
-        <HStack w={'400px'} h={'200px'} mb={'300px'}>
-          <Skeleton borderRadius={'10px'} w={'70px'} h={'70px'} />
-          <VStack>
-            <HStack color={'white'}>
-              <SkeletonText fontSize={'24px'}>{gameName}</SkeletonText>
-            </HStack>
-          </VStack>
-        </HStack>
-      ) : (
-        <ProfileMiniBox
-          puuid={summonerData?.puuid}
-          gameName={gameName}
-          tagLine={tagLine}
-          accountId={summonerData?.accountId}
-          profileIconId={summonerData?.profileIconId}
-          summonerId={summonerData?.summonerId}
-          summonerLevel={summonerData?.summonerLevel}
-        />
-      )}
-    </VStack>
+    <Container maxW="full" display="flex" alignItems="center" justifyContent="center">
+      <VStack mt={20}>
+        {isSummonerDataLoading ? (
+          <HStack w={'400px'} h={'200px'} mb={'300px'}>
+            <Skeleton borderRadius={'10px'} w={'70px'} h={'70px'} />
+            <VStack>
+              <HStack color={'white'}>
+                <SkeletonText fontSize={'24px'}>{gameName}</SkeletonText>
+              </HStack>
+            </VStack>
+          </HStack>
+        ) : (
+          <ProfileMiniBox
+            puuid={summonerData?.puuid}
+            gameName={gameName}
+            tagLine={tagLine}
+            accountId={summonerData?.accountId}
+            profileIconId={summonerData?.profileIconId}
+            summonerId={summonerData?.summonerId}
+            summonerLevel={summonerData?.summonerLevel}
+          />
+        )}
+      </VStack>
+    </Container>
   );
 }
